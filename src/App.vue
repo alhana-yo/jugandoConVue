ad759ac4f5e0b2ebdf4894f8999b52d28c86bf69
Beatriz German <beatriz.german@kairosds.com>
2020-01-13 10:42:28 +0100

    First exercise done

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Crea tu lista!</h1>
+    <input type="text" v-model="value" placeholder="Valor a añadir en la lista" />
+
+    <ul class="list">
+      <li class="listElement" v-for="value in valuesArray" :key="value">{{value}}</li>
+    </ul>
+    <button @click="setValue(value)">Agregar a la lista</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data: () => {
+    return {
+      valuesArray: [],
+      value: ""
+    };
+  },
+  methods: {
+    setValue: function(value) {
+      this.valuesArray.push(value);
+      this.value = "";
+    }
+  }
+};
+</script>
